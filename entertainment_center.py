a94125c2a7edeb6c09a0df388d83eae6c9553817
Denis Dumulong <dumulong@sbcglobal.net>
2018-11-04 19:21:25 -0600

    redefining the links

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,35 +1,38 @@
 import fresh_tomatoes
 import media
 
+wikimedia = "http://upload.wikimedia.org/wikipedia"
+youTube = "https://www.youtube.com/watch?v="
+
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys taht come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+                        wikimedia + "/en/1/13/Toy_Story.jpg",
+                        youTube + "KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
-                        "A marine on an alien planet",
-                        "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                        "https://www.youtube.com/watch?v=-9ceBgWV8io")
+                     "A marine on an alien planet",
+                     wikimedia + "/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     youTube + "-9ceBgWV8io")
 
 school_of_rock = media.Movie("School Of Rock",
                         "Using Rock Music To Learn",
-                        "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                        "https://www.youtube.com/watch?v=3PsUJFEBC74")
+                        wikimedia + "/en/1/11/School_of_Rock_Poster.jpg",
+                        youTube + "3PsUJFEBC74")
 
 ratatouille = media.Movie("Ratatouille",
                         "A rat is a chef in Paris",
-                        "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                        "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+                        wikimedia + "/en/5/50/RatatouillePoster.jpg",
+                        youTube + "c3sBBRxDAqk")
 
 midnight_in_paris = media.Movie("Midnight In Paris",
                         "Going back in time to meet authors",
-                        "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                        "https://www.youtube.com/watch?v=FAfR8omt-CY")
+                        wikimedia + "/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                        youTube + "FAfR8omt-CY")
 
 hunger_games = media.Movie("Hunger Games",
                         "A really real reality show",
-                        "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                        "https://www.youtube.com/watch?v=PbA63a7H0bo")
+                        wikimedia + "/en/4/42/HungerGamesPoster.jpg",
+                        youTube + "PbA63a7H0bo")
 
 
 
