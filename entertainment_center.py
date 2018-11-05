6ce7375e037319c2210a0c9c75ec2ecbe4cf411f
Denis Dumulong <dumulong@sbcglobal.net>
2018-11-04 18:44:30 -0600

    Adding the other movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,15 +3,35 @@ import media
 
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys taht come to life",
-                        "http://upload.wikimedia.org/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-
-print (toy_story.storyline)
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                         "A marine on an alien planet",
-                        "http://upload.wikimedia.org/Avatar-Teaser-Poster.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+                        "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                        "https://www.youtube.com/watch?v=-9ceBgWV8io")
 
-movies = [toy_story, avatar]
+school_of_rock = media.Movie("School Of Rock",
+                        "Using Rock Music To Learn",
+                        "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                        "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+                        "A rat is a chef in Paris",
+                        "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                        "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_paris = media.Movie("Midnight In Paris",
+                        "Going back in time to meet authors",
+                        "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+                        "https://www.youtube.com/watch?v=FAfR8omt-CY")
+
+hunger_games = media.Movie("Hunger Games",
+                        "A really real reality show",
+                        "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+                        "https://www.youtube.com/watch?v=PbA63a7H0bo")
+
+
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
 fresh_tomatoes.open_movies_page(movies)
